bbbf8eb3ce9ba9a81c4256a58fcefe33606a56b2
RimaSusan <susanrima@gmail.com>
2019-09-07 09:56:50 -0400

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,62 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+
+function ufoTable(data) {
+    tbody.html("");
+// Step 1: Loop Through `data` and console.log each data object
+data.forEach(function(UFOdata) {
+    console.log("insideUfoTable")
+    
+   // console.log(UFOdata);
+// Step 2:  Use d3 to append one table row `tr` for each data object
+    var row = tbody.append("tr");
+// Step 3:  Use `Object.entries` to console.log each data value
+    Object.entries(UFOdata).forEach(function([key, value]) {
+        //console.log(key, value);
+// Step 4: Use d3 to append 1 cell per data value (date, city, state, country, shape, duration, comments)
+    var cell = row.append("td");
+    cell.text(value);
+ });
+});
+}
+
+//create a function for filtering the table using a date input from user
+
+function filterdate() {
+    var inputDate = d3.select("#datetime");
+    var inputDateValue = inputDate.property("value");
+      console.log(inputDateValue);
+
+ if (inputDateValue) {
+    var filteredData = tableData.filter(row => row.datetime === inputDateValue);
+    console.log(filteredData.length);
+    //if else to return an alert message incase no data is retrieved for the date entered.
+    if (filteredData.length) {
+   
+        console.log("inputDateEntered");
+        // console.log(filteredData);
+         ufoTable(filteredData);
+    }
+else {
+    alert("No Data found for the entered date. Try another date");
+}
+ }
+ //Alert the user to enter a date before filtering
+ else {
+     alert("Please enter a date to filter");
+ }
+
+}
+
+//select the filter button & call it if there is a click action identified.
+var filterbutton = d3.select("#filter-btn");
+
+filterbutton.on("click", filterdate);
+ufoTable(tableData);
+
+
